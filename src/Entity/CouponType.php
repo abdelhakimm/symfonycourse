cf72bc6fa77a3fed13c3a8d12d2a5db35f607535
abdelhakimm <abdelhakim.moustabchir@gmail.com>
2023-04-13 15:38:59 +0200

    make entity coupon

--- src/Entity/CouponType.php
+++ src/Entity/CouponType.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CouponTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CouponTypeRepository::class)]
@@ -15,6 +17,14 @@ class CouponType
 
     #[ORM\Column(length: 50)]
     private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'coupon_type', targetEntity: Coupon::class, orphanRemoval: true)]
+    private Collection $coupons;
+
+    public function __construct()
+    {
+        $this->coupons = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class CouponType
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Coupon>
+     */
+    public function getCoupons(): Collection
+    {
+        return $this->coupons;
+    }
+
+    public function addCoupon(Coupon $coupon): self
+    {
+        if (!$this->coupons->contains($coupon)) {
+            $this->coupons->add($coupon);
+            $coupon->setCouponType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCoupon(Coupon $coupon): self
+    {
+        if ($this->coupons->removeElement($coupon)) {
+            // set the owning side to null (unless already changed)
+            if ($coupon->getCouponType() === $this) {
+                $coupon->setCouponType(null);
+            }
+        }
 
         return $this;
     }
